6305c877b883bf5209a3ad24ecafcf91ee524cb0
GiancarloNavarro <30733744+GiancarloNavarro@users.noreply.github.com>
2018-05-12 16:33:51 -0600

    splash screen

---
+++ FrontEnd/Productos/app/src/main/java/com/example/casca/productos/Activity/SplashScreen.java
@@ -0,0 +1,60 @@
+package com.example.casca.productos.Activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.casca.productos.ConnectionHelper.JsonConnection;
+import com.example.casca.productos.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity{
+    private Timer timer;
+    private ProgressBar progressBar;
+    private int i=0;
+    TextView textView;
+    public static final String url="http://10.0.2.2:8080/Servlet/Servlet?accion=consultarProductos";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        progressBar=(ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setProgress(0);
+        textView=(TextView)findViewById(R.id.textView);
+        textView.setText("");
+
+        JsonConnection jconexion = new JsonConnection();
+        jconexion.execute(new String[]{url,"GET"});
+
+        final long period = 50;
+        timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                //this repeats every 100 ms
+                if (i<100){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                        }
+                    });
+                    progressBar.setProgress(i);
+                    i++;
+                }else{
+                    //closing the timer
+                    timer.cancel();
+                    Intent intent =new Intent(SplashScreen.this,ProductosList.class);
+                    startActivity(intent);
+                    // close this activity
+                    finish();
+                }
+            }
+        }, 0, period);
+    }
+
+}
